8ef6c2be9b72e8878164ed45e3ba1eaf7317c2e7
Ann Naser Nabil <ann.n.nabil@gmail.com>
2026-03-26 04:36:23 +0600

    added

--- src/app/api/tags/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getAllTags } from '@/lib/posts';
-
-export async function GET(request: NextRequest) {
-  const tags = getAllTags();
-  return NextResponse.json({ tags });
-}
